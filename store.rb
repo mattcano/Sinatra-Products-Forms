562757560d424e7ba59a9ce93c956bd416364fcb
Matt Cano <cano.matt@gmail.com>
2013-04-05 15:51:11 -0700

    incomplete save

--- store.rb
+++ store.rb
@@ -4,6 +4,10 @@ gem 'sinatra', '1.3.0'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sqlite3'
+require 'open-uri'
+require 'json'
+# require 'better_errors'
+require 'uri'
  
 before do
   @db = SQLite3::Database.new "store.sqlite3"
@@ -41,38 +45,46 @@ get '/products' do
   erb :show_products
 end 
 
-get "/products/:id" do
-  id = params[:id]
-  @row = @db.get_first_row("SELECT * FROM products WHERE id = #{id};")
-  erb :update_products
-end
-
-post "/update_products" do
-  id = params[:id]
-  name = params[:product_name]
-  price = params[:product_price]
-  sale = params[:sale]
-  sql = "UPDATE products SET name = '#{name}', price = #{price}, on_sale = '#{sale}' WHERE id = #{id}"
-  @rs = @db.execute(sql)
-  # db2 = SQLite3::Database.new "store.sqlite3"
-  # db.results_as_hash = true
-  @rs2 = @db.execute("SELECT * FROM products;")
-  #make the product in to a db
-  erb :product_updated
+get "/products/search" do
+  @q = params[:q]
+  file = open("http://search.twitter.com/search.json?q=#{URI.escape(@q)}")
+  @results = JSON.load(file.read)
+  erb :search_results
 end
 
 get "/products/:id/destroy" do
-  id = params[:id]
-  @id = id
+  @id = params[:id]
   erb :product_deleted
 end
 
 post "/products/:id/destroy" do
-  id = params[:id]
-  @id = id
-  sql = "DELETE FROM products WHERE id = #{id}"
+  @id = params[:id]
+  sql = "DELETE FROM products WHERE id = #{@id}"
   @rs = @db.execute(sql)
+  @rs2 = @db.execute("SELECT * FROM products;")
+  erb :product_deleted
 end
+
+get "/products/:id" do
+  @id = params[:id]
+  @row = @db.get_first_row("SELECT * FROM products WHERE id = #{@id};")
+  erb :update_products
+end
+
+post "/products/:id" do
+  id = params[:id]
+  name = params[:product_name]
+  price = params[:product_price]
+  sale = params[:sale]
+  sql = "UPDATE products SET name = '#{name}', price = #{price}, on_sale = '#{sale}' WHERE id = #{id.to_i}"
+  @rs = @db.execute(sql)
+  @rs2 = @db.execute("SELECT * FROM products;")
+  erb :product_updated
+end
+
+
+
+
 
 =begin
  
